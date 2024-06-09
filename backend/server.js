9cd7c0a8a7cd7e57f555cc3849b316de831295bb
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-06-09 10:57:11 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.listen(8000, () => {
+  console.log("Server is running on port ");
+});
